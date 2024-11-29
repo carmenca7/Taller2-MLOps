f127b1668ea387f2b21b015b9da6fc442679dc56
carmenca7 <carvajalgutierrezc@gmail.com>
2024-11-28 22:33:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,101 +9,101 @@ pipeline = load('bankruptcy_pipeline.joblib')
 
 # Definir las variables de entrada al modelo
 class BankruptcyInput(BaseModel):
-    roa_c_before_interest_and_depreciation_before_interest: float
-    roa_a_before_interest_and_percentage_after_tax: float
-    roa_b_before_interest_and_depreciation_after_tax: float
-    operating_gross_margin: float
-    realized_sales_gross_margin: float
-    operating_profit_rate: float
-    pre_tax_net_interest_rate: float
-    after_tax_net_interest_rate: float
-    non_industry_income_and_expenditure_to_revenue: float
-    continuous_interest_rate_after_tax: float
-    operating_expense_rate: float
-    research_and_development_expense_rate: float
-    cash_flow_rate: float
-    interest_bearing_debt_interest_rate: float
-    tax_rate_a: float
-    net_value_per_share_b: float
-    net_value_per_share_a: float
-    net_value_per_share_c: float
-    persistent_eps_last_four_seasons: float
-    cash_flow_per_share: float
-    revenue_per_share: float
-    operating_profit_per_share: float
-    per_share_net_profit_before_tax: float
-    realized_sales_gross_profit_growth_rate: float
-    operating_profit_growth_rate: float
-    after_tax_net_profit_growth_rate: float
-    regular_net_profit_growth_rate: float
-    continuous_net_profit_growth_rate: float
-    total_asset_growth_rate: float
-    net_value_growth_rate: float
-    total_asset_return_growth_rate_ratio: float
-    cash_reinvestment_percentage: float
-    current_ratio: float
-    quick_ratio: float
-    interest_expense_ratio: float
-    total_debt_to_total_net_worth: float
-    debt_ratio_percentage: float
-    net_worth_to_assets: float
-    long_term_fund_suitability_ratio_a: float
-    borrowing_dependency: float
-    contingent_liabilities_to_net_worth: float
-    operating_profit_to_paid_in_capital: float
-    net_profit_before_tax_to_paid_in_capital: float
-    inventory_and_accounts_receivable_to_net_value: float
-    total_asset_turnover: float
-    accounts_receivable_turnover: float
-    average_collection_days: float
-    inventory_turnover_rate: float
-    fixed_assets_turnover_frequency: float
-    net_worth_turnover_rate: float
-    revenue_per_person: float
-    operating_profit_per_person: float
-    allocation_rate_per_person: float
-    working_capital_to_total_assets: float
-    quick_assets_to_total_assets: float
-    current_assets_to_total_assets: float
-    cash_to_total_assets: float
-    quick_assets_to_current_liability: float
-    cash_to_current_liability: float
-    current_liability_to_assets: float
-    operating_funds_to_liability: float
-    inventory_to_working_capital: float
-    inventory_to_current_liability: float
-    current_liabilities_to_liability: float
-    working_capital_to_equity: float
-    current_liabilities_to_equity: float
-    long_term_liability_to_current_assets: float
-    retained_earnings_to_total_assets: float
-    total_income_to_total_expense: float
-    total_expense_to_assets: float
-    current_asset_turnover_rate: float
-    quick_asset_turnover_rate: float
-    working_capital_turnover_rate: float
-    cash_turnover_rate: float
-    cash_flow_to_sales: float
-    fixed_assets_to_assets: float
-    current_liability_to_liability: float
-    current_liability_to_equity: float
-    equity_to_long_term_liability: float
-    cash_flow_to_total_assets: float
-    cash_flow_to_liability: float
-    cfo_to_assets: float
-    cash_flow_to_equity: float
-    current_liability_to_current_assets: float
-    liability_assets_flag: float
-    net_income_to_total_assets: float
-    total_assets_to_gnp_price: float
-    no_credit_interval: float
-    gross_profit_to_sales: float
-    net_income_to_stockholders_equity: float
-    liability_to_equity: float
-    degree_of_financial_leverage: float
-    interest_coverage_ratio: float
-    net_income_flag: float
-    equity_to_liability: float
+    ROA(C) before interest and depreciation before interest: float
+    ROA(A) before interest and % after tax: float
+    ROA(B) before interest and depreciation after tax: float
+    Operating Gross Margin: float
+    Realized Sales Gross Margin: float
+    Operating Profit Rate: float
+    Pre-tax net Interest Rate: float
+    After-tax net Interest Rate: float
+    Non-industry income and expenditure/revenue: float
+    Continuous interest rate (after tax): float
+    Operating Expense Rate: float
+    Research and development expense rate: float
+    Cash flow rate: float
+    Interest-bearing debt interest rate: float
+    Tax rate (A): float
+    Net Value Per Share (B): float
+    Net Value Per Share (A): float
+    Net Value Per Share (C): float
+    Persistent EPS in the Last Four Seasons: float
+    Cash Flow Per Share: float
+    Revenue Per Share (Yuan Â¥): float
+    Operating Profit Per Share (Yuan Â¥): float
+    Per Share Net profit before tax (Yuan Â¥): float
+    Realized Sales Gross Profit Growth Rate: float
+    Operating Profit Growth Rate: float
+    After-tax Net Profit Growth Rate: float
+    Regular Net Profit Growth Rate: float
+    Continuous Net Profit Growth Rate: float
+    Total Asset Growth Rate: float
+    Net Value Growth Rate: float
+    Total Asset Return Growth Rate Ratio: float
+    Cash Reinvestment %: float
+    Current Ratio: float
+    Quick Ratio: float
+    Interest Expense Ratio: float
+    Total debt/Total net worth: float
+    Debt ratio %: float
+    Net worth/Assets: float
+    Long-term fund suitability ratio (A): float
+    Borrowing dependency: float
+    Contingent liabilities/Net worth: float
+    Operating profit/Paid-in capital: float
+    Net profit before tax/Paid-in capital: float
+    Inventory and accounts receivable/Net value: float
+    Total Asset Turnover: float
+    Accounts Receivable Turnover: float
+    Average Collection Days: float
+    Inventory Turnover Rate (times): float
+    Fixed Assets Turnover Frequency: float
+    Net Worth Turnover Rate (times): float
+    Revenue per person: float
+    Operating profit per person: float
+    Allocation rate per person: float
+    Working Capital to Total Assets: float
+    Quick Assets/Total Assets: float
+    Current Assets/Total Assets: float
+    Cash/Total Assets: float
+    Quick Assets/Current Liability: float
+    Cash/Current Liability: float
+    Current Liability to Assets: float
+    Operating Funds to Liability: float
+    Inventory/Working Capital: float
+    Inventory/Current Liability: float
+    Current Liabilities/Liability: float
+    Working Capital/Equity: float
+    Current Liabilities/Equity: float
+    Long-term Liability to Current Assets: float
+    Retained Earnings to Total Assets: float
+    Total income/Total expense: float
+    Total expense/Assets: float
+    Current Asset Turnover Rate: float
+    Quick Asset Turnover Rate: float
+    Working capitcal Turnover Rate: float
+    Cash Turnover Rate: float
+    Cash Flow to Sales: float
+    Fixed Assets to Assets: float
+    Current Liability to Liability: float
+    Current Liability to Equity: float
+    Equity to Long-term Liability: float
+    Cash Flow to Total Assets: float
+    Cash Flow to Liability: float
+    CFO to Assets: float
+    Cash Flow to Equity: float
+    Current Liability to Current Assets: float
+    Liability-Assets Flag: int
+    Net Income to Total Assets: float
+    Total assets to GNP price: float
+    No-credit Interval: float
+    Gross Profit to Sales: float
+    Net Income to Stockholder's Equity: float
+    Liability to Equity: float
+    Degree of Financial Leverage (DFL): float
+    Interest Coverage Ratio (Interest expense to EBIT): float
+    Net Income Flag: int
+    Equity to Liability: float
 
 # Iniciar FastAPI app
 app = FastAPI()
